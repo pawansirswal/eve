befe47d1ec3f9822e4dbe9a59025e8b0a19d7d1b
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-07 16:01:55 +0530

    07-10-2022

--- src/pages/Dashboard/MiniWidgets.js
+++ src/pages/Dashboard/MiniWidgets.js
@@ -47,9 +47,9 @@ class MiniWidgets extends Component {
     document.body.classList.add("no_padding");
   }
   tog_large() {
-    this.setState(() => ({
+    this.setState({
       modal_large: !this.state.modal_large,
-    }));
+    });
     console.log('------------------------------Clicked---------------------------')
     this.removeBodyCss();
   }
@@ -213,22 +213,40 @@ class MiniWidgets extends Component {
                 </Media>
               </CardBody>
 
-              <Modal
+              {/* <Modal
                 size="lg"
                 isOpen={this.state.modal_large}
                 toggle={this.tog_large}
               >
                 <ModalHeader
-                  toggle={() => {
-                    this.setState({ tog_large: false });
-                  }}
+                  toggle={
+                    this.tog_large
+                  }
                 >
                   Modal Header{" "}
                 </ModalHeader>
                 <ModalBody>
-                  <p>body</p>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem error voluptatem minus, magni laborum qui officia praesentium, fugiat enim, obcaecati nobis expedita a nesciunt optio molestiae. Modi voluptatibus doloribus esse.</p>
                 </ModalBody>
-              </Modal>
+              </Modal> */}
+
+              <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                      ...
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
               <CardBody className="border-top py-3">
                 <div className="text-truncate">
@@ -536,7 +554,12 @@ class MiniWidgets extends Component {
                       </div>
                     </Media>
                     <div className="text-primary">
-                      <i className={"ri-stack-line font-size-24"}></i>
+                      <i className={"ri-stack-line font-size-24"}
+                        onClick={() => {
+                          this.tog_large();
+                        }}>
+                      </i>
+                      {/* <i className={"ri-stack-line font-size-24"} data-bs-toggle="modal" data-bs-target="#exampleModal"></i> */}
                     </div>
                   </Media>
                 </CardBody>
